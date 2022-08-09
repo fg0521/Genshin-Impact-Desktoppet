c2c9f4da9232af1fe1d8796e0110b8cdf2310086
slash <1364435561@qq.com>
2022-08-09 23:02:29 +0800

    slash 新增了背景音乐和人物语音 修改部分代码

--- desktoppet.py
+++ desktoppet.py
@@ -1,5 +1,8 @@
 import os
+import random
 import sys
+import time
+
 import pygame.mixer as mixer
 from PyQt5.QtCore import QTimer, Qt, QSize, QEvent
 from PyQt5.QtGui import QPixmap, QFont, QIcon, QImage, QCursor
@@ -11,12 +14,15 @@ from config import args, dic
 class Pet(QWidget):
     def __init__(self):
         super(Pet, self).__init__()
-        self.file_name = args.kl  # 文件夹名称
-        self.file_list = sorted(os.listdir(self.file_name))  # 对文件夹里面的所有图片进行排序
-        self.img = QImage().load(os.path.join(self.file_name, str(self.file_list[1])))  # 获取第一张图片作为托盘图标
+        self.role_name = args.kl  # 文件夹名称
+        self.pre_path = args.path
+        self.file_path = self.pre_path + self.role_name
+        self.file_list = sorted(os.listdir(self.file_path))  # 对文件夹里面的所有图片进行排序
+        self.img = QImage().load(os.path.join(self.file_path, str(self.file_list[1])))  # 获取第一张图片作为托盘图标
         self.wt = 300
         self.ht = 300
-        self.music_player = False
+        self.audio_player = False
+        self.bgmusic_player = False
         self.tp = QSystemTrayIcon(self)  # 初始化系统托盘
         self.tp.setToolTip('原来你也玩原神')
         self.init_window()
@@ -26,7 +32,6 @@ class Pet(QWidget):
         self.is_follow_mouse = False  # 初始化鼠标没有移动
         self.mouse_drag_pos = self.pos()
 
-
     def act(self):
         """
         读取图片不同的地址，实现动画效果
@@ -35,7 +40,7 @@ class Pet(QWidget):
             self.index += 1
         else:
             self.index = 1
-        self.pic_url = os.path.join(self.file_name, str(self.file_list[self.index]))
+        self.pic_url = os.path.join(self.file_path, str(self.file_list[self.index]))
         self.pm = QPixmap(self.pic_url)
         self.lbl.setPixmap(self.pm)
 
@@ -43,18 +48,18 @@ class Pet(QWidget):
         """
         初始化窗口
         """
-        self.setToolTip(dic[self.file_name])
+        # self.setToolTip(dic[self.role_name])
         self.setGeometry(0, 400, self.wt, self.ht)  # 设置窗口和位置
         self.lbl = QLabel(self)  # 初始化一个QLabel对象
         self.index = 1
-        self.pic_url = os.path.join(self.file_name, str(self.file_list[self.index]))
+        self.pic_url = os.path.join(self.file_path, str(self.file_list[self.index]))
         self.pm = QPixmap(self.pic_url)  # 图像显示
         self.lbl.setPixmap(self.pm)  # 设置Qlabel为一个Pimap图片
         self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)  # 设置窗口置顶以及去掉边框
         self.setAutoFillBackground(False)  # 设置窗口背景透明
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.show()  # 显示窗口
-        self.tp.setIcon(QIcon(os.path.join(self.file_name, str(self.file_list[1]))))  # 系统托盘图标
+        self.tp.setIcon(QIcon(os.path.join(self.file_path, str(self.file_list[1]))))  # 系统托盘图标
         self.timer = QTimer()
         self.timer.timeout.connect(self.act)
 
@@ -66,48 +71,70 @@ class Pet(QWidget):
         self.menu = self.bar.addMenu('菜单')
 
         self.pets = self.menu.addMenu('人物')
-        bbl = QAction('芭芭拉', self)
-        bbl.triggered.connect(self.bbl)
-        kq = QAction('刻晴', self)
-        kq.triggered.connect(self.kq)
-        dlk = QAction('迪卢克', self)
-        dlk.triggered.connect(self.dlk)
-        kl = QAction('可莉', self)
-        kl.triggered.connect(self.kl)
-        gy = QAction('甘雨', self)
-        gy.triggered.connect(self.gy)
 
+        # self.pets.addAction(QAction(r, self,triggered=lambda: self.reshow(r)))
+
+        bbl = QAction('芭芭拉', self)
+        bbl.triggered.connect(lambda: self.reshow('芭芭拉'))
         self.pets.addAction(bbl)
+        kq = QAction('刻晴', self)
+        kq.triggered.connect(lambda: self.reshow('刻晴'))
         self.pets.addAction(kq)
+        dlk = QAction('迪卢克', self)
+        dlk.triggered.connect(lambda: self.reshow('迪卢克'))
         self.pets.addAction(dlk)
+        kl = QAction('可莉', self)
+        kl.triggered.connect(lambda: self.reshow('可莉'))
         self.pets.addAction(kl)
+        gy = QAction('甘雨', self)
+        gy.triggered.connect(lambda: self.reshow('甘雨'))
         self.pets.addAction(gy)
+        q = QAction('琴', self)
+        q.triggered.connect(lambda: self.reshow('琴'))
+        self.pets.addAction(q)
+
+        self.audio = QAction('语音开', self)
+        self.audio.triggered.connect(self.audio_switch)
+        self.menu.addAction(self.audio)
+
+        self.bgmusics = self.menu.addMenu('背景音乐')
+        bg_md = QAction('蒙徳', self)
+        bg_md.triggered.connect(lambda: self.bg_music('蒙徳'))
+        self.bgmusics.addAction(bg_md)
+        bg_ly = QAction('璃月', self)
+        bg_ly.triggered.connect(lambda: self.bg_music('璃月'))
+        self.bgmusics.addAction(bg_ly)
+        bg_dq = QAction('稻妻', self)
+        bg_dq.triggered.connect(lambda: self.bg_music('稻妻'))
+        self.bgmusics.addAction(bg_dq)
+        bg_off = QAction('关闭', self)
+        bg_off.triggered.connect(lambda: self.bg_music('关闭'))
+        self.bgmusics.addAction(bg_off)
 
         show = QAction('显示', self)
         show.triggered.connect(self.showwin)
-        self.music = QAction('音乐开', self)
-        self.music.triggered.connect(self.music_switch)
+        self.menu.addAction(show)
+
         quit = QAction('退出', self)
         quit.triggered.connect(self.quit)
-
-        self.menu.addAction(show)
-        self.menu.addAction(self.music)
         self.menu.addAction(quit)
 
         self.tp.setContextMenu(self.menu)
         self.tp.show()
 
-    def reshow(self, name):
+    def reshow(self, role_name):
         """
         桌面宠物的替换
         """
+        print(role_name)
         self.close()  # 关闭窗口
         self.lbl.deleteLater()  # 清空Label
+        self.role_name = role_name
         self.wt = 300
         self.ht = 300
-        self.file_name = name
-        self.file_list = sorted(os.listdir(self.file_name))
-        self.img = QImage().load(os.path.join(self.file_name, str(self.file_list[1])))  # 获取第一张图片作为托盘图标
+        self.file_path = self.pre_path + role_name
+        self.file_list = sorted(os.listdir(self.file_path))
+        self.img = QImage().load(os.path.join(self.file_path, str(self.file_list[1])))  # 获取第一张图片作为托盘图标
 
         # scale = 1.2  # 每次放到20%
         # img = QImage(self.pic_url)  # 创建图片实例
@@ -117,31 +144,7 @@ class Pet(QWidget):
         # self.pm = QPixmap(pixImg)  # 图像显示
         self.init_window()
         self.tp.show()
-
-    def bbl(self):
-        # 芭芭拉
-        self.reshow('yuanshen/芭芭拉')
         self.timer.start(50)
-
-    def gy(self):
-        # 甘雨
-        self.reshow('yuanshen/甘雨')
-        self.timer.start(50)
-
-    def kq(self):
-        # 刻晴
-        self.reshow('yuanshen/刻晴')
-        self.timer.start(50)
-
-    def dlk(self):
-        # 迪卢克
-        self.reshow('yuanshen/迪卢克')
-        self.timer.start(60)
-
-    def kl(self):
-        # 可莉
-        self.reshow('yuanshen/可莉')
-        self.timer.start(60)
 
     def mousePressEvent(self, event):
         # 鼠标左键事件
@@ -185,24 +188,27 @@ class Pet(QWidget):
     def showwin(self):
         self.setWindowOpacity(1)
 
-    def enterEvent(self, event):
-        if event.type() == QEvent.Enter:
-            self.paly_music(self.file_name)
-
-    def music_switch(self):
-        self.music_player = not self.music_player
-        if self.music_player:
-            self.music.setText('音乐关')
+    def audio_switch(self):
+        self.audio_player = not self.audio_player
+        self.audio.setText('语音关') if self.audio_player else self.audio.setText('语音开')
+        while self.audio_player:
+            audio_list = os.listdir(f'./music/{self.role_name}')
+            random_audio = audio_list[random.randint(0, len(audio_list) - 1)]
+            mixer.init()
+            mixer.music.load(f'./music/{self.role_name}/{random_audio}')
+            if not mixer.music.get_busy():
+                mixer.music.play()
+            time.sleep(random.randint(15, 100))
         else:
-            self.music.setText('音乐开')
+            mixer.music.stop()
 
-    def paly_music(self, s):
-        if self.music_player:
-            if s in ['bbl', 'kl', 'kq']:
-                mixer.init()
-                mixer.music.load('./music/' + s + '.mp3')
-                if not mixer.music.get_busy():
-                    mixer.music.play()
+    def bg_music(self, area):
+        if area == '关闭':
+            mixer.Sound(f'./music/{self.area}/background.mp3').stop()
+        else:
+            mixer.init()
+            mixer.Sound(f'./music/{area}/background.mp3').play()
+            self.area = area
 
 
 if __name__ == '__main__':
--- download_audio.py
+++ download_audio.py
@@ -1,4 +1,8 @@
+import re
+
 import requests
+import pandas as pd
+import os
 def download(url,path):
     res = requests.get(url, stream=True)
     if res.status_code == 200:
@@ -6,14 +10,24 @@ def download(url,path):
             for chunk in res.iter_content():
                 f.write(chunk)
         f.close()
+        print(f'{path}下载完成...')
     else:
         print('链接不可访问...')
 
 if __name__ == '__main__':
-    url = 'https://uploadstatic.mihoyo.com/ys-obc/2021/03/12/4359827/83e894b9faa4ebe8935d75a5c54c959c_3883502189516950902.mp3'
-    path = 'test.mp3'
-    download(url,path)
+    roles = ['可莉','芭芭拉','刻晴','万叶','迪卢克','阿贝多','八重神子','班尼特','达达利亚','迪奥娜','菲谢尔','甘雨','胡桃','荒泷一斗','雷电将军','莫娜','七七','琴','神里绫华','神里绫人','温迪','宵宫','魈','心海','行秋','夜兰','优菈','早柚','钟离']
+    df = pd.read_csv('/Users/maoyufeng/slash/ys_audio_cp.csv')
+    df = df[df['title'].str.contains('闲聊') | df['title'].str.contains('早上好') | df['title'].str.contains('中午好')
+            | df['title'].str.contains('晚上好') | df['title'].str.contains('晚安')]
+    df = df[df['name'].isin(roles)]
+    for _,row in df.iterrows():
+        name = row[1]
+        title = re.sub(r'[^\u4e00-\u9fa5]+','',row[2])
+        url = row[3]
+        if not os.path.exists(f'./music/{name}'):
+            os.mkdir(f'./music/{name}')
+        download(url=url,path=f'./music/{name}/{title}.mp3')
 
-
+    # print(df)
 
 
