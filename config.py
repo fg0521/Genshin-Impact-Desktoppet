9f4f2b99b18c867a68815e9290b6bbfeedb8f324
slash <1364435561@qq.com>
2022-08-24 22:34:03 +0800

    slash:update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--music',default='蒙徳')
 parser.add_argument('--audio',default=True)
-parser.add_argument('--img_path', default='yuanshen')
+parser.add_argument('--img_path', default='png')
 parser.add_argument('--music_path', default='music')
 parser.add_argument('--role', default='可莉')
 args = parser.parse_args()
--- rename.py
+++ rename.py
@@ -13,10 +13,13 @@ def add_zero(name,num=4):
 
 
 if __name__ == '__main__':
-    path = './yuanshen/优菈'
+    path = './png/胡桃'
     img_list = [i for i in os.listdir(path) if i.endswith('png')]
     for img in img_list:
-        name = img.split('-')[1]
-        name = add_zero(name)
-        os.rename(os.path.join(path,img),os.path.join(path,name))
+        try:
+            name = img.split('-')[1]
+            name = add_zero(name)
+            os.rename(os.path.join(path,img),os.path.join(path,name))
+        except:
+            pass
     # print(add_zero('123.jpg'))
